5d37883b4794675c5e064754a73775d6ccb54e77
ahtungko <itko1993@gmail.com>
2023-12-24 21:20:02 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,4 +1,4 @@
-export const OWNER = "blacksev";
+export const OWNER = "ahtungko";
 export const REPO = "Gemini-Next-Web";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;

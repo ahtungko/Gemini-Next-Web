6a1f863227a2cb7277af429f09727de5562fd232
ahtungko <itko1993@gmail.com>
2023-12-30 23:18:11 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -9,8 +9,8 @@ export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export const DEFAULT_CORS_HOST = "https://fxcku.link";
-export const DEFAULT_API_HOST = `https://generativelanguage.googleapis.com`;
-export const OPENAI_BASE_URL = "https://generativelanguage.googleapis.com";
+export const DEFAULT_API_HOST = `https://generativelanguage.googleapis.com/`;
+export const OPENAI_BASE_URL = "https://generativelanguage.googleapis.com/";
 
 export enum Path {
   Home = "/",

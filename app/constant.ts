e62e8eef8a37cc5b38ee3adaf4da909f0b606af3
ahtungko <itko1993@gmail.com>
2023-12-31 02:38:26 +0800

    rollback

--- app/constant.ts
+++ app/constant.ts
@@ -8,9 +8,9 @@ export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/c
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
-export const DEFAULT_CORS_HOST = "http://fxcku.link";
-export const DEFAULT_API_HOST = `http://generativelanguage.googleapis.com/v1/models/gemini-pro:streamGenerateContent`;
-export const OPENAI_BASE_URL = "http://generativelanguage.googleapis.com/v1/models/gemini-pro:streamGenerateContent";
+export const DEFAULT_CORS_HOST = "https://gemini-chat.pro";
+export const DEFAULT_API_HOST = `https://api.gemini-chat.pro`;
+export const OPENAI_BASE_URL = "https://api.gemini-chat.pro";
 
 export enum Path {
   Home = "/",
